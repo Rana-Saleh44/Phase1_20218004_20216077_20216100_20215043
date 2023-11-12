b8e1cdce52f4eab2fce8cd05f4a4816f54932235
Moaz <132786697+Moaz266@users.noreply.github.com>
2023-11-12 20:40:21 +0200

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,38 @@
+public class Account
+{
+    private String username;
+    private String password;
+    private String mobileNumber;
+    private String accountNumber;
+    private double balance;
+
+    public Account(String username, String password, String mobileNumber, String accountNumber)
+    {
+        this.username = username;
+        this.password = password;
+        this.mobileNumber = mobileNumber;
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+    }
+
+    public void transferToWallet(String walletMobileNumber, double amount)
+    {
+
+    }
+
+    public void transferToInstapayAccount(String accountUsername, double amount)
+    {
+
+    }
+
+    public double inquireBalance()
+    {
+        return balance;
+    }
+
+    public void payBill(String billType)
+    {
+
+    }
+}
+
