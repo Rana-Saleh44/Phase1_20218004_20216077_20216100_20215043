4a386fb02915643804130867d46a0b038c3b7809
Rana-Saleh44 <ranamhsaleh@gmail.com>
2023-11-13 18:42:09 +0200

    bill Payment deleted

--- GasBillPayment.java
+++
@@ -1,7 +0,0 @@
-public class GasBillPayment implements BillPaymentStrategy{
-    @Override
-    public void payBill(Bill bill){
-        System.out.println("Paying Gas Bill: " +bill.getBillId() + " from Account: ");
-    }
-
-}
--- WaterBillPayment.java
+++
@@ -1,7 +0,0 @@
-public class WaterBillPayment implements BillPaymentStrategy{
-    @Override
-    public void payBill(Bill bill){
-        System.out.println("Paying Water Bill: " +bill.getBillId() + " from Account: ");
-    }
-
-}
